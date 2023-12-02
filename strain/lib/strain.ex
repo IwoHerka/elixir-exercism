f229ad37a270f9ba8eb6199c7c2570c706258f2a
Iwo Herka <iwo.herka@makimo.pl>
2023-12-02 11:35:31 +0100

    test: add solution to strain

--- strain/lib/strain.ex
+++ strain/lib/strain.ex
@@ -1,10 +1,15 @@
 defmodule Strain do
+
   @doc """
   Given a `list` of items and a function `fun`, return the list of items where
   `fun` returns true.
 
   Do not use `Enum.filter`.
   """
+  def keep([], _fun) do
+    []
+  end
+
   @spec keep(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def keep([head | tail], fun) do
     if fun.(head) do
@@ -12,10 +17,6 @@ defmodule Strain do
     else
       keep(tail, fun)
     end
-  end
-
-  def keep([], _fun) do
-    []
   end
 
   @doc """
@@ -26,6 +27,6 @@ defmodule Strain do
   """
   @spec discard(list :: list(any), fun :: (any -> boolean)) :: list(any)
   def discard(list, fun) do
-    keep(list, &(!fun.(&1)))
+    keep(list, fn e -> !fun.(e) end)
   end
 end
